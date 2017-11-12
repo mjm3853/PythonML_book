49f6c48bd84df0285c7cbf8e5c35722afc86ce29
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-11-12 18:57:39 -0500

    partitioning

--- Ch4/data.py
+++ Ch4/data.py
@@ -2,24 +2,28 @@ import pandas as pd
 import numpy as np
 from io import StringIO
 
-
+print("----------------------------------------")
+print('Loaded Data')
+print("----------------------------------------")
 csv_data = '''A,B,C,D
 1.0,2.0,3.0,4.0
 5.0,6.0,,8.0
 10.0,11.0,12.0,'''
 df = pd.read_csv(StringIO(csv_data))
 print(df)
-print("----------------------------------------")
 
-# Imputer
+print("----------------------------------------")
+print('Imputer')
+print("----------------------------------------")
 from sklearn.preprocessing import Imputer
 imr = Imputer(missing_values="NaN", strategy="mean", axis=0)
 imr = imr.fit(df)
 imputed_data = imr.transform(df.values)
 print(imputed_data)
-print("----------------------------------------")
 
-# Categorical Data
+print("----------------------------------------")
+print('Categorical Data')
+print("----------------------------------------")
 dx = pd.DataFrame([
     ['green', 'M', 10.1, 'class1'],
     ['red', 'L', 13.5, 'class2'],
@@ -27,9 +31,10 @@ dx = pd.DataFrame([
 ])
 dx.columns = ['color', 'size', 'price', 'classlabel']
 print(dx)
-print("----------------------------------------")
 
-# Ordinal Features
+print("----------------------------------------")
+print('Ordinal Features')
+print("----------------------------------------")
 size_mapping = {
     'XL': 3,
     'L': 2,
@@ -37,62 +42,90 @@ size_mapping = {
 }
 dx['size'] = dx['size'].map(size_mapping)
 print(dx)
-print("----------------------------------------")
 
 '''
-# Reverse Ordinal Features
+print("----------------------------------------")
+print('Reverse Ordinal Features')
+print("----------------------------------------")
 inv_size_mapping = {
     v: k for k, v in size_mapping.items()
 }
 dx['size'] = dx['size'].map(inv_size_mapping)
 print(dx)
-print("----------------------------------------")
 '''
 
-# Encoding Class Labels
+print("----------------------------------------")
+print('Encoding Class Labels')
+print("----------------------------------------")
 class_mapping = {
     label: idx for idx, label in
     enumerate(np.unique(dx['classlabel']))
 }
 print(class_mapping)
-print("----------------------------------------")
 
-# Transform Class Labels
+print("----------------------------------------")
+print('Transform Class Labels')
+print("----------------------------------------")
 dx['classlabel'] = dx['classlabel'].map(class_mapping)
 print(dx)
-print("----------------------------------------")
 
-# Inverse Class Mapping
+print("----------------------------------------")
+print('Inverse Class Mapping')
+print("----------------------------------------")
 inv_class_mapping = {
     v: k for k, v in class_mapping.items()
 }
 dx['classlabel'] = dx['classlabel'].map(inv_class_mapping)
 print(dx)
-print("----------------------------------------")
 
-# Class Encoder
+print("----------------------------------------")
+print('Class Encoder')
+print("----------------------------------------")
 from sklearn.preprocessing import LabelEncoder
 class_le = LabelEncoder()
 y = class_le.fit_transform(dx['classlabel'].values)
 print(y)
-print("----------------------------------------")
 
-# Inverse Class Encoder
+print("----------------------------------------")
+print('Inverse Class Encoder')
+print("----------------------------------------")
 class_le.inverse_transform(y)
 print(y)
-print("----------------------------------------")
 
-# Color Encoder
+print("----------------------------------------")
+print('Color Encoder')
+print("----------------------------------------")
 X = dx[['color', 'size', 'price']].values
 color_le = LabelEncoder()
 X[:, 0] = color_le.fit_transform(X[:, 0])
 print(X)
-print("----------------------------------------")
 
-# One Hot Encoding
+print("----------------------------------------")
+print('One Hot Encoding')
+print("----------------------------------------")
 from sklearn.preprocessing import OneHotEncoder
 ohe = OneHotEncoder(categorical_features=[0])
 print(ohe.fit_transform(X).toarray())
 
-# Get Dummies
+print("----------------------------------------")
+print('Get Dummies')
+print("----------------------------------------")
 print(pd.get_dummies(dx[['price', 'color', 'size']]))
+
+print("----------------------------------------")
+print('Load Wine Dataset')
+print("----------------------------------------")
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity', 'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+print('Class labels', np.unique(df_wine['Class label']))
+print("----------------------------------------")
+print(df_wine.head())
+
+print("----------------------------------------")
+print('Partition Wine Dataset')
+print("----------------------------------------")
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:,0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+print('Partition completed')
