c17a4bf59dde0815ad75783a0f5595552a1d4652
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-11-12 18:34:28 -0500

    more data stuff

--- Ch4/data.py
+++ Ch4/data.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 from io import StringIO
+
+
 csv_data = '''A,B,C,D
 1.0,2.0,3.0,4.0
 5.0,6.0,,8.0
@@ -27,4 +30,69 @@ print(dx)
 print("----------------------------------------")
 
 # Ordinal Features
+size_mapping = {
+    'XL': 3,
+    'L': 2,
+    'M': 1
+}
+dx['size'] = dx['size'].map(size_mapping)
+print(dx)
+print("----------------------------------------")
 
+'''
+# Reverse Ordinal Features
+inv_size_mapping = {
+    v: k for k, v in size_mapping.items()
+}
+dx['size'] = dx['size'].map(inv_size_mapping)
+print(dx)
+print("----------------------------------------")
+'''
+
+# Encoding Class Labels
+class_mapping = {
+    label: idx for idx, label in
+    enumerate(np.unique(dx['classlabel']))
+}
+print(class_mapping)
+print("----------------------------------------")
+
+# Transform Class Labels
+dx['classlabel'] = dx['classlabel'].map(class_mapping)
+print(dx)
+print("----------------------------------------")
+
+# Inverse Class Mapping
+inv_class_mapping = {
+    v: k for k, v in class_mapping.items()
+}
+dx['classlabel'] = dx['classlabel'].map(inv_class_mapping)
+print(dx)
+print("----------------------------------------")
+
+# Class Encoder
+from sklearn.preprocessing import LabelEncoder
+class_le = LabelEncoder()
+y = class_le.fit_transform(dx['classlabel'].values)
+print(y)
+print("----------------------------------------")
+
+# Inverse Class Encoder
+class_le.inverse_transform(y)
+print(y)
+print("----------------------------------------")
+
+# Color Encoder
+X = dx[['color', 'size', 'price']].values
+color_le = LabelEncoder()
+X[:, 0] = color_le.fit_transform(X[:, 0])
+print(X)
+print("----------------------------------------")
+
+# One Hot Encoding
+from sklearn.preprocessing import OneHotEncoder
+ohe = OneHotEncoder(categorical_features=[0])
+print(ohe.fit_transform(X).toarray())
+
+# Get Dummies
+print(pd.get_dummies(dx[['price', 'color', 'size']]))
