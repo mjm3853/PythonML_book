6155671a55d2b89980d9f0d737a29be2eb67dda1
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-11-15 20:46:43 -0500

    feature transformation

--- Ch5/dimensions.py
+++ Ch5/dimensions.py
@@ -3,6 +3,7 @@
 """Dimensionality Reduction Examples from Chapter 5"""
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -23,3 +24,37 @@ print('\nX Tested and Standardized:\n\n', X_test_std[:3])
 cov_mat = np.cov(X_train_std.T)
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 print('\nEigenvalues:\n\n%s' % eigen_vals)
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+
+plt.bar(range(1, 14), var_exp, alpha=0.5, align='center',
+        label='individual explained variance')
+plt.step(range(1, 14), cum_var_exp, where='mid',
+         label='cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+plt.legend(loc='best')
+plt.show()
+
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+eigen_pairs.sort(reverse=True)
+print('\nEigenpairs:\n\n%s' % eigen_pairs)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis]))
+print('\nMatrix:\n\n%s' % w)
+
+X_train_pca = X_train_std.dot(w)
+
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0],
+                X_train_pca[y_train == l, 1], c=c, label=l, marker=m)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.show()
