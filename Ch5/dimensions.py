f3cadd51eea49ae80c33efae59bffc1e69983e30
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-09 18:32:14 -0500

    shortcuts

--- Ch5/dimensions.py
+++ Ch5/dimensions.py
@@ -4,11 +4,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.datasets import make_moons
 from plot_decision_regions_three import plot_decision_regions_three
+from rbf_kernel_pca import rbf_kernel_pca
 
 df_wine = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
@@ -148,4 +152,41 @@ for l, c, m in zip(np.unique(y_train), colors, markers):
 plt.xlabel('LD 1')
 plt.ylabel('LD 2')
 plt.legend(loc='lower right')
+plt.show()
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_train_lda = lda.fit_transform(X_train_std, y_train)
+
+lr = LogisticRegression()
+lr = lr.fit(X_train_lda, y_train)
+plot_decision_regions_three(X_train_lda, y_train, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower right')
+plt.show()
+
+X_test_lda = lda.transform(X_test_std)
+plot_decision_regions_three(X_test_lda, y_test, classifier=lr)
+plt.xlabel('LD 1')
+plt.ylabel('LD 2')
+plt.legend(loc='lower right')
+plt.show()
+
+X, y = make_moons(n_samples=100, random_state=123)
+plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
+plt.show()
+
+scikit_pca = PCA(n_components=2)
+X_spca = scikit_pca.fit_transform(X)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
 plt.show()
