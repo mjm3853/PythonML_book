eb271da884ba166aa6c3ac6b0c4647ce050d72b8
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-09 18:38:11 -0500

    boom boom

--- Ch5/dimensions.py
+++ Ch5/dimensions.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -189,4 +190,19 @@ ax[0].set_ylabel('PC2')
 ax[1].set_ylim([-1, 1])
 ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
+plt.show()
+
+X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
+ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_kpca[y == 0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_kpca[y == 1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 plt.show()
