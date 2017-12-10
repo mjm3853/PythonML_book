b80a4f4389e6aed73ff7ab03905791bb1660ea6a
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-09 19:43:53 -0500

    kernel

--- Ch5/dimensions.py
+++ Ch5/dimensions.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
+from sklearn.decomposition import KernelPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.datasets import make_moons
 from plot_decision_regions_three import plot_decision_regions_three
@@ -181,11 +182,15 @@ plt.show()
 
 scikit_pca = PCA(n_components=2)
 X_spca = scikit_pca.fit_transform(X)
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
-ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
-ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
-ax[1].scatter(X_spca[y == 0, 0], np.zeros((50,1))+0.02, color='red', marker='^', alpha=0.5)
-ax[1].scatter(X_spca[y == 1, 0], np.zeros((50,1))-0.02, color='blue', marker='o', alpha=0.5)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((50, 1)) +
+              0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((50, 1)) -
+              0.02, color='blue', marker='o', alpha=0.5)
 ax[0].set_xlabel('PC1')
 ax[0].set_ylabel('PC2')
 ax[1].set_ylim([-1, 1])
@@ -193,6 +198,7 @@ ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 plt.show()
 
+"""
 X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
 ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
@@ -207,6 +213,7 @@ ax[1].set_xlabel('PC1')
 ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 plt.show()
+"""
 
 X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
 plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
@@ -215,11 +222,15 @@ plt.show()
 
 scikit_pca = PCA(n_components=2)
 X_spca = scikit_pca.fit_transform(X)
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
-ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
-ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
-ax[1].scatter(X_spca[y == 0, 0], np.zeros((500,1))+0.02, color='red', marker='^', alpha=0.5)
-ax[1].scatter(X_spca[y == 1, 0], np.zeros((500,1))-0.02, color='blue', marker='o', alpha=0.5)
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1],
+              color='red', marker='^', alpha=0.5)
+ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1],
+              color='blue', marker='o', alpha=0.5)
+ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1)) +
+              0.02, color='red', marker='^', alpha=0.5)
+ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1)) -
+              0.02, color='blue', marker='o', alpha=0.5)
 ax[0].set_xlabel('PC1')
 ax[0].set_ylabel('PC2')
 ax[1].set_ylim([-1, 1])
@@ -227,6 +238,7 @@ ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 plt.show()
 
+"""
 X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,3))
 ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
@@ -240,4 +252,45 @@ ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
+"""
+
+X, y = make_moons(n_samples=100, random_state=123)
+alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+x_new = X[25]
+print(x_new)
+x_proj = alphas[25]
+print(x_proj)
+
+
+def project_x(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row)**2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
+
+
+x_reproj = project_x(x_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+print(x_reproj)
+
+plt.scatter(alphas[y == 0, 0], np.zeros((50)),
+            color='red', marker='^', alpha=0.5)
+plt.scatter(alphas[y == 1, 0], np.zeros((50)),
+            color='blue', marker='o', alpha=0.5)
+plt.scatter(x_proj, 0, color='black',
+            label='original projection of point X(25)', marker='^', s=100)
+plt.scatter(x_reproj, 0, color='green',
+            label='remapped point X(25)', marker='x', s=500)
+plt.legend(scatterpoints=1)
+plt.show()
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.show()
--- Ch5/rbf_kernel_pca.py
+++ Ch5/rbf_kernel_pca.py
@@ -22,8 +22,11 @@ def rbf_kernel_pca(X, gamma, n_components):
 
     Returns
     -----------------
-    X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+    alphas: {NumPy ndarray}, shape = [n_samples, k_features]
         Projected dataset
+
+    lambdas: list
+        Eigenvalues
     """
 
     # Calculate pairwise squared Euclidean distances
@@ -46,7 +49,10 @@ def rbf_kernel_pca(X, gamma, n_components):
     eigvals, eigvecs = eigh(K)
 
     # Collect the top k eigenvectors (projected samples)
-    X_pc = np.column_stack((eigvecs[:, -i]
+    alphas = np.column_stack((eigvecs[:, -i]
                             for i in range(1, n_components + 1)))
 
-    return X_pc
+    # Collect the corresponding eigenvalues
+    lambdas = [eigvals[-i] for i in range(1,n_components+1)]
+    
+    return alphas, lambdas
