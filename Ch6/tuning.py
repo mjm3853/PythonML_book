871986dad2311c4208a54594e470d89fe3bb0c5b
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-10 19:55:32 -0500

    more again

--- Ch6/tuning.py
+++ Ch6/tuning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -7,6 +8,7 @@ from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import StratifiedKFold, cross_val_score
+from sklearn.learning_curve import learning_curve
 
 
 df = pd.read_csv(
@@ -45,3 +47,26 @@ scores = cross_val_score(estimator=pipe_lr, X=X_train,
 print('CV accuracy scores: %s' % scores)
 print('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
 
+pipe_lr = Pipeline([('scl', StandardScaler()),
+                    ('clf', LogisticRegression(penalty='l2', random_state=1))])
+
+train_sizes, train_scores, test_scores = learning_curve(
+    estimator=pipe_lr, X=X_train, y=y_train, train_sizes=np.linspace(0.1, 1.0, 10), cv=10, n_jobs=-1)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+plt.plot(train_sizes, train_mean, color='blue', marker='o',
+         markersize=5, label='training accuracy')
+plt.fill_between(train_sizes, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15, color='blue')
+plt.plot(train_sizes, test_mean, color='green', linestyle='--',
+         marker='s', markersize=5, label='validation accuracy')
+plt.fill_between(train_sizes, test_mean + test_std,
+                 test_mean - test_std, alpha=0.15, color='green')
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.0])
+plt.show()
