2321b118ef71154403193cd02aedaa420a3d251e
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-10 21:06:22 -0500

    dunzo

--- Ch6/tuning.py
+++ Ch6/tuning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interp
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -9,7 +10,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import StratifiedKFold, cross_val_score
 from sklearn.learning_curve import learning_curve
-
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, make_scorer, roc_curve, auc
 
 df = pd.read_csv(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
@@ -69,4 +73,90 @@ plt.xlabel('Number of training samples')
 plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
 plt.ylim([0.8, 1.0])
+plt.show()
+
+pipe_svc = Pipeline([('scl', StandardScaler()),
+                     ('clf', SVC(random_state=1))])
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+param_grid = [{'clf__C': param_range,
+               'clf__kernel': ['linear']},
+              {'clf__C': param_range,
+               'clf__gamma': param_range,
+               'clf__kernel': ['rbf']}]
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid,
+                  scoring='accuracy', cv=10, n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
+
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid,
+                  scoring='accuracy', cv=2, n_jobs=-1)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=5)
+print('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0), param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}],
+                  scoring='accuracy', cv=5)
+scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=2)
+print('CV Accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+plt.xlabel('predicated label')
+plt.ylabel('true label')
+plt.show()
+
+print('Precision: %.3f' % precision_score(y_true=y_test, y_pred=y_pred))
+print('Recall: %.3f' % recall_score(y_true=y_test, y_pred=y_pred))
+print('F1: %.3f' % f1_score(y_true=y_test, y_pred=y_pred))
+
+scorer = make_scorer(f1_score, pos_label=0)
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid,
+                  scoring=scorer, cv=10)
+
+pipe_lr = Pipeline([('scl', StandardScaler()), ('pca', PCA(
+    n_components=2)), ('clf', LogisticRegression(penalty='l2', random_state=0, C=100.0))])
+X_train2 = X_train[:, [4, 14]]
+cv = StratifiedKFold(y_train, n_folds=3, random_state=1)
+fig = plt.figure(figsize=(7, 5))
+mean_tpr = 0.0
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+
+for i, (train, test) in enumerate(cv):
+    probas = pipe_lr.fit(
+        X_train2[train], y_train[train]).predict_proba(X_train2[test])
+    fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label=1)
+    mean_tpr = interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' %
+             (i + 1, roc_auc))
+
+plt.plot([0, 1], [0, 1], linestyle='--',
+         color=(0.6, 0.6, 0.6), label='random guessing')
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, 'k--',
+         label='mean ROC (area = %0.2f)' % mean_auc, lw=2)
+plt.plot([0, 0, 1], [0, 1, 1], lw=2, linestyle=':',
+         color='black', label='perfect performance')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('false positive rate')
+plt.ylabel('true positive rate')
+plt.title('Receiver Operator Characteristic')
+plt.legend(loc='lower right')
 plt.show()
