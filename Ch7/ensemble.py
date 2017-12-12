a5b5bd01630d74ffc9c9a5d4faf938e7ca16b5e3
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-11 23:45:35 -0500

    somehow working

--- Ch7/ensemble.py
+++ Ch7/ensemble.py
@@ -2,6 +2,15 @@ from scipy.misc import comb
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from MajorityVoteClassifier import MajorityVoteClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.cross_validation import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
 
 
 def ensemble_error(n_classifier, error):
@@ -10,17 +19,52 @@ def ensemble_error(n_classifier, error):
              (n_classifier - k) for k in range(k_start, n_classifier + 1)]
     return sum(probs)
 
+
 ee = ensemble_error(n_classifier=11, error=0.25)
 
 print("Ensemble Error: ", ee)
 
 error_range = np.arange(0.0, 1.01, 0.01)
-ens_errors = [ensemble_error(n_classifier=11, error=error) for error in error_range]
+ens_errors = [ensemble_error(n_classifier=11, error=error)
+              for error in error_range]
 
 plt.plot(error_range, ens_errors, label='Ensemble error', linewidth=2)
-plt.plot(error_range, error_range, linestyle='--', label='Base error', linewidth=2)
+plt.plot(error_range, error_range, linestyle='--',
+         label='Base error', linewidth=2)
 plt.xlabel('Base Error')
 plt.ylabel('Base/Ensemble Error')
 plt.legend(loc='upper left')
 plt.grid()
 plt.show()
+
+iris = datasets.load_iris()
+X, y = iris.data[50:, [1, 2]], iris.target[50:]
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=1)
+
+clf1 = LogisticRegression(penalty='l2', C=0.001, random_state=0)
+clf2 = DecisionTreeClassifier(max_depth=1, criterion='entropy', random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1, p=2, metric='minkowski')
+pipe1 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf3]])
+clf_labels = ['Logistic Regression', 'Decision Tree', 'KNN']
+print('10-fold cross validation\n')
+for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train,
+                             y=y_train, cv=10, scoring='roc_auc')
+    print("ROC AUC: %0.2f (+/- %0.2f) [%s]" %
+          (scores.mean(), scores.std(), label))
+
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+clf_labels += ['Majority Voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train,
+                             y=y_train, cv=10, scoring='roc_auc')
+    print("Accuracy: %0.2f (+/- %0.2f) [%s]" %
+          (scores.mean(), scores.std(), label))
