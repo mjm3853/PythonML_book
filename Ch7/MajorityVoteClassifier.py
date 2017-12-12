83ad3af23f6558e2ce64f03ac78385b361b4d61e
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-11 22:55:10 -0500

    hope its right

--- Ch7/MajorityVoteClassifier.py
+++ Ch7/MajorityVoteClassifier.py
@@ -38,7 +38,7 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
                                   value in _name_estimators(classifiers)}
         self.vote = vote
         self.weights = weights
-    
+
     def fit(self, X, y):
         """ Fit classifiers
 
@@ -68,3 +68,65 @@ class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
             fitted_clf = clone(clf).fit(X, self.labelenc_.transform(y))
             self.classifiers_.append(fitted_clf)
         return self
+
+    def predict(self, X):
+        """ Predict class labels for X.
+
+        Parameters
+        --------------
+        X: {array-like, sparse matrix},
+            Shape = [n_samples, n_features]
+            Matrix of training samples.
+
+        Returns
+        -------------
+        maj_vote : array-like, shape = [n_samples]
+            Predicted class labels.
+
+        """
+        if self.vote == 'probability':
+            maj_vote = np.argmax(self.predict_proba(X), axis=1)
+
+        else:
+            predictions = np.asarray([clf.predict(X)
+                                      for clf in self.classifiers_]).T
+
+        maj_vote = np.apply_along_axis(lambda x: np.argmax(
+            np.bincount(x, weights=self.weights)), axis=1, arr=predictions)
+
+        maj_vote = self.labelenc_.inverse_transform(maj_vote)
+        return maj_vote
+
+    def predict_proba(self, X):
+        """ Predict class probabilities for X.
+
+        Parameters
+        ---------------
+        X : {array-like, sparse matrix},
+            shape = [n_samples, n_features]
+            Training vectors, where n_samples is
+            the number of samples and
+            n_features is the number of features.abs
+
+        Returns
+        -------------
+        avg_proba : array-like, shape = [n_samples, n_classes]
+        Weighted average probability for each class per sample.
+
+        """
+        probas = np.asarray([clf.predict_proba(X)
+                             for clf in self.classifiers_])
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
+        return avg_proba
+
+    def get_params(self, deep=True):
+        """ Get classifier parameter names for GridSearch """
+        if not deep:
+            return super(MajorityVoteClassifier, self).get_params(deep=False)
+
+        else:
+            out = self.named_classifiers.copy()
+            for name, step in six.iteritems(self.named_classifiers):
+                for key, value in six.iteritems(step.get_params(deep=True)):
+                    out['%s__%s' % (name, key)] = value
+        return out
