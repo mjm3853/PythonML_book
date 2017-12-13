ca338d946122c32b2b8a705e5ab13ea1a2c43586
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-12 20:51:44 -0500

    ensamble

--- Ch7/ensemble.py
+++ Ch7/ensemble.py
@@ -2,6 +2,7 @@ from scipy.misc import comb
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from itertools import product
 from MajorityVoteClassifier import MajorityVoteClassifier
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -11,6 +12,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_curve, auc
 
 
 def ensemble_error(n_classifier, error):
@@ -68,3 +70,48 @@ for clf, label in zip(all_clf, clf_labels):
                              y=y_train, cv=10, scoring='roc_auc')
     print("Accuracy: %0.2f (+/- %0.2f) [%s]" %
           (scores.mean(), scores.std(), label))
+
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, color=clr, linestyle=ls,
+             label='%s (auc = %0.2f)' % (label, roc_auc))
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1], linestyle='--', color='gray', linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid()
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.show()
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=2, ncols=2, sharex='col',
+                        sharey='row', figsize=(7, 5))
+for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+    z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    z = z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, z, alpha=0.3)
+    axarr[idx[0], idx[1]].scatter(
+        X_train_std[y_train == 0, 0], X_train_std[y_train == 0, 1], c='blue', marker='^', s=50)
+    axarr[idx[0], idx[1]].scatter(
+        X_train_std[y_train == 1, 0], X_train_std[y_train == 1, 1], c='red', marker='o', s=50)
+    axarr[idx[0], idx[1]].set_title(tt)
+plt.text(-3.5, -4.5, s='Sepal width [std]',
+         ha='center', va='center', fontsize=12)
+plt.text(-10.5, 4.5, s='Petal length [std]',
+         ha='center', va='center', fontsize=12, rotation=90)
+plt.show()
+
+# print(mv_clf.get_params())
