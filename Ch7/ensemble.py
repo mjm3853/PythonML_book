34875feafbdeb542355ba3d7c07c35d521463683
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-14 19:43:59 -0500

    ensemble

--- Ch7/ensemble.py
+++ Ch7/ensemble.py
@@ -2,6 +2,7 @@ from scipy.misc import comb
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import pandas as pd
 from itertools import product
 from MajorityVoteClassifier import MajorityVoteClassifier
 from sklearn import datasets
@@ -12,7 +13,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, accuracy_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble import BaggingClassifier
 
 
 def ensemble_error(n_classifier, error):
@@ -115,3 +118,77 @@ plt.text(-10.5, 4.5, s='Petal length [std]',
 plt.show()
 
 # print(mv_clf.get_params())
+
+params = {'decisiontreeclassifier__max_depth': [
+    1, 2], 'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
+grid = GridSearchCV(estimator=mv_clf, param_grid=params,
+                    cv=10, scoring='roc_auc')
+grid.fit(X_train, y_train)
+for params, mean_score, scores in grid.grid_scores_:
+    print('%0.3f+/-%0.2f %r' % (mean_score, scores.std() / 2, params))
+
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+df_wine.columns = ['Class label',
+                   'Alcohol',
+                   'Malic acid',
+                   'Ash',
+                   'Alcalinity of ash',
+                   'Magnesium',
+                   'Total phenols',
+                   'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity',
+                   'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+df_wine = df_wine[df_wine['Class label'] != 1]
+y = df_wine['Class label'].values
+X = df_wine[['Alcohol', 'Hue']].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.40, random_state=1)
+
+tree = DecisionTreeClassifier(
+    criterion="entropy", max_depth=None, random_state=1)
+bag = BaggingClassifier(base_estimator=tree, n_estimators=500, max_samples=1.0,
+                        max_features=1.0, bootstrap=True, bootstrap_features=False, n_jobs=1, random_state=1)
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f' %
+      (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f' % (bag_train, bag_test))
+
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=1, ncols=2, sharex='col',
+                        sharey='row', figsize=(8, 3))
+for idx, clf, tt in zip([0, 1], [tree, bag], ['Decision Tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+    z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    z = z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, z, alpha=0.3)
+    axarr[idx].scatter(
+        X_train[y_train == 0, 0], X_train[y_train == 0, 1], c='blue', marker='^')
+    axarr[idx].scatter(
+        X_train[y_train == 1, 0], X_train[y_train == 1, 1], c = 'red', marker = 'o')
+    axarr[idx].set_title(tt)
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -1.2, s = 'Hue',
+         ha = 'center', va = 'center', fontsize = 12)
+plt.show()
