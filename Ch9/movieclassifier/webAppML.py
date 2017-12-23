76fca8511fce25cf43b9ecbaa518a84161878932
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-23 18:08:59 -0500

    move directory

--- Ch9/movieclassifier/webAppML.py
+++ Ch9/movieclassifier/webAppML.py
@@ -9,4 +9,5 @@ clf = pickle.load(open(os.path.join('pk1_objects', 'classifier.pk1'), 'rb'))
 label = {0: 'negative', 1: 'positive'}
 example = ['I hate how much I love this movie']
 X = vect.transform(example)
-print('Prediction: %s\nProbability: %.2f%%' % (label[clf.predict(X)[0]], np.max(clf.predict_proba(X))*100))
+print('Prediction: %s\nProbability: %.2f%%' %
+      (label[clf.predict(X)[0]], np.max(clf.predict_proba(X)) * 100))
