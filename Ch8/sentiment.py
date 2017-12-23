40bde964b0d5292cebe96d69ce05b77dfe8ed89a
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-23 12:27:01 -0500

    ch8 done

--- Ch8/sentiment.py
+++ Ch8/sentiment.py
@@ -1,12 +1,23 @@
 import re
 import pandas as pd
 import numpy as np
+import pyprind
+pbar = pyprind.ProgBar(45)
 import nltk
 nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
 
 
+df = pd.read_csv('./movie_data.csv')
+
+stop = stopwords.words('english')
+
+"""
 def preprocessor(text):
     text = re.sub('<[^>]*>', '', text)
     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
@@ -14,17 +25,12 @@ def preprocessor(text):
         ' '.join(emoticons).replace('-', '')
     return text
 
-
-df = pd.read_csv('./movie_data.csv')
-
 df['review'] = df['review'].apply(preprocessor)
 
 print(df.head(3))
 
-
 def tokenizer(text):
     return text.split()
-
 
 run = 'runners like running and thus they run'
 
@@ -32,12 +38,97 @@ print(tokenizer(run))
 
 porter = PorterStemmer()
 
-
 def tokenizer_porter(text):
     return [porter.stem(word) for word in text.split()]
 
-
 print(tokenizer_porter(run))
 
-stop = stopwords.words('english')
+
 print([w for w in tokenizer_porter(run)[-10:] if w not in stop])
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+tfidf = TfidfVectorizer(strip_accents=None, lowercase=False, preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]
+               },
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf': [False],
+               'vect__norm': [None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]
+               }
+              ]
+lr_tfidf = Pipeline(
+    [('vect', tfidf), ('clf', LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, scoring='accuracy', cv=5, verbose=1, n_jobs=1)
+gs_lr_tfidf.fit(X_train, y_train)
+
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+clf = gs_lr_tfidf.best_estimator_
+print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
+"""
+
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text).lower() + \
+        ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+
+def stream_docs(path):
+    with open(path, 'r', encoding='utf-8') as csv:
+        next(csv)  # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+
+print(next(stream_docs(path='./movie_data.csv')))
+
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+
+vect = HashingVectorizer(decode_error='ignore', n_features=(
+    2 ** 21), preprocessor=None, tokenizer=tokenizer)
+clf = SGDClassifier(loss='log', random_state=1, n_iter=1)
+doc_stream = stream_docs(path='./movie_data.csv')
+
+classes = np.array([0, 1])
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+    pbar.update()
+
+X_test, y_test = get_minibatch(doc_stream, size=5000)
+X_test = vect.transform(X_test)
+print('Accuracy: %.3f' % clf.score(X_test, y_test))
+
+clf = clf.partial_fit(X_test, y_test)
