83fe56a5ebd86aa4fb1a09c6650f7756cde430a7
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-24 12:37:15 -0500

    robust

--- Ch10/regression.py
+++ Ch10/regression.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import numpy as np
 from LinearRegressionGD import LinearRegressionGD
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, RANSACRegressor
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
                  header=None, sep='\s+')
@@ -39,10 +39,12 @@ plt.tight_layout()
 # plt.savefig('./figures/cost.png', dpi=300)
 plt.show()
 
+
 def lin_regplot(X, y, model):
     plt.scatter(X, y, c='blue')
     plt.plot(X, model.predict(X), color='red')
     return None
+
 
 lin_regplot(X_std, y_std, lr)
 plt.xlabel('Average number of rooms [RM] (standardized)')
@@ -65,3 +67,22 @@ lin_regplot(X, y, slr)
 plt.xlabel('Average number of rooms [RM]')
 plt.ylabel('Price in $1000\'s [MEDV]')
 plt.show()
+
+ransac = RANSACRegressor(LinearRegression(), max_trials=100, min_samples=50, residual_metric=lambda x: np.sum(
+    np.abs(x), axis=1), residual_threshold=5.0, random_state=0)
+ransac.fit(X, y)
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+plt.scatter(X[inlier_mask], y[inlier_mask],
+            c='blue', marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask],
+            c='lightgreen', marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='red')
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000\'s [MEDV]')
+plt.show()
+
+print('Slope: %.3f' % ransac.estimator_.coef_[0])
+print('Intercept: %.3f' % ransac.estimator_.intercept_)
