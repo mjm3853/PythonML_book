2fea60181a5727ee14e51fa4109d48822a5febd8
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-24 12:51:11 -0500

    evaluate lr

--- Ch10/regression.py
+++ Ch10/regression.py
@@ -5,6 +5,7 @@ import numpy as np
 from LinearRegressionGD import LinearRegressionGD
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, RANSACRegressor
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
                  header=None, sep='\s+')
@@ -86,3 +87,22 @@ plt.show()
 
 print('Slope: %.3f' % ransac.estimator_.coef_[0])
 print('Intercept: %.3f' % ransac.estimator_.intercept_)
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+slr = LinearRegression()
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+plt.scatter(y_train_pred, y_train_pred - y_train,
+            c='blue', marker='o', label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test,
+            c='lightgreen', marker='s', label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
+plt.show()
