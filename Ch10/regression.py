d776bebb94db2674c075a671c19d057b0d3dde77
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-24 12:06:45 -0500

    linear regression

--- Ch10/regression.py
+++ Ch10/regression.py
@@ -1,6 +1,39 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-df = read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
-              header=None, sep='\s+')
+import seaborn as sns
+import numpy as np
+from LinearRegressionGD import LinearRegressionGD
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
+                 header=None, sep='\s+')
 df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM',
               'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
 df.head()
+
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+sns.pairplot(df[cols], size=2.5)
+plt.show()
+
+cm = np.corrcoef(df[cols].values.T)
+sns.set(font_scale=1.5)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt=".2f", annot_kws={
+                 'size': 15}, yticklabels=cols, xticklabels=cols)
+plt.show()
+
+X = df[['RM']].values
+y = df['MEDV'].values
+
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+X_std = sc_x.fit_transform(X)
+y_std = sc_y.fit_transform(y[:, np.newaxis]).flatten()
+lr = LinearRegressionGD()
+lr.fit(X_std, y_std)
+
+plt.plot(range(1, lr.n_iter + 1), lr.cost_)
+plt.ylabel('SSE')
+plt.xlabel('Epoch')
+plt.tight_layout()
+# plt.savefig('./figures/cost.png', dpi=300)
+plt.show()
