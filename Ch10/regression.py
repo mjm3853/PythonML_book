6dc06d79e3e7f4192afe36e2f3df3c8bf79803b2
Matthew McGrath <matthew.mcgrath12@gmail.com>
2017-12-24 13:57:25 -0500

    tree

--- Ch10/regression.py
+++ Ch10/regression.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.linear_model import LinearRegression, RANSACRegressor, Ridge, Lasso, ElasticNet
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
                  header=None, sep='\s+')
@@ -170,9 +172,12 @@ y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
 cubic_r2 = r2_score(y, regr.predict(X_cubic))
 
 plt.scatter(X, y, label='training points', color='lightgray')
-plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' % linear_r2, color='blue', lw=2, linestyle=':')
-plt.plot(X_fit, y_quad_fit, label='quadratic (d=2), $R^2=%.2f$' % quadratic_r2, color='red', lw=2, linestyle='-')
-plt.plot(X_fit, y_cubic_fit, label='cubic (d=3), $R^2=%.2f$' % cubic_r2, color='green', lw=2, linestyle='--')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' %
+         linear_r2, color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_quad_fit, label='quadratic (d=2), $R^2=%.2f$' %
+         quadratic_r2, color='red', lw=2, linestyle='-')
+plt.plot(X_fit, y_cubic_fit, label='cubic (d=3), $R^2=%.2f$' %
+         cubic_r2, color='green', lw=2, linestyle='--')
 plt.xlabel('[LSTAT]')
 plt.ylabel('[MEDV]')
 plt.legend(loc='upper right')
@@ -181,14 +186,51 @@ plt.show()
 X_log = np.log(X)
 y_sqrt = np.sqrt(y)
 
-X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+X_fit = np.arange(X_log.min() - 1, X_log.max() + 1, 1)[:, np.newaxis]
 regr = regr.fit(X_log, y_sqrt)
 y_lin_fit = regr.predict(X_fit)
 linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
 
 plt.scatter(X_log, y_sqrt, label='training points', color='lightgray')
-plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' % linear_r2, color='blue', lw=2, linestyle=':')
+plt.plot(X_fit, y_lin_fit, label='linear (d=1), $R^2=%.2f$' %
+         linear_r2, color='blue', lw=2, linestyle=':')
 plt.xlabel('[LSTAT]')
 plt.ylabel('[MEDV]')
 plt.legend(loc='upper right')
+plt.show()
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+tree = DecisionTreeRegressor(max_depth=3)
+tree.fit(X, y)
+sort_idx = X.flatten().argsort()
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('[LSTAT]')
+plt.ylabel('[MEDV]')
+plt.show()
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=1)
+forest = RandomForestRegressor(
+    n_estimators=1000, criterion='mse', random_state=1, n_jobs=-1)
+forest.fit(X_train, y_train)
+y_train_pred = forest.predict(X_train)
+y_test_pred = forest.predict(X_test)
+
+print('MSE train: %.3f, test: %.3f' % (mean_squared_error(
+    y_train, y_train_pred), mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (r2_score(
+    y_train, y_train_pred), r2_score(y_test, y_test_pred)))
+
+plt.scatter(y_train_pred, y_train_pred - y_train,
+            c='black', marker='o', s=35, alpha=0.5, label='Training data')
+plt.scatter(y_test_pred, y_test_pred - y_test,
+            c='lightgreen', marker='s', s=35, alpha=0.7, label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+plt.xlim([-10, 50])
 plt.show()
